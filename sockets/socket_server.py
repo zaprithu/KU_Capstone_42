882eb4e5941cbc959cf29daf22a18ba0ebcce9a9
James Hanselman <68711757+JamesHansIV@users.noreply.github.com>
2025-04-08 23:39:57 -0500

    bugfixes on socketserver

--- sockets/socket_server.py
+++ sockets/socket_server.py
@@ -1,6 +1,7 @@
-import socket
 import SocketServer
 import json
+import signal 
+import sys
 
 # get from robot
 IP = '192.168.0.28'
@@ -17,73 +18,58 @@ class Track:
             "song_name": self.song_name
         }
 
-def start_server(host=IP, port=PORT):
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((host, port))
-
-    print("Server listening on {}:{}".format(host, port))
-
-
-    while True:
-        # Receive data from the client
-        data, addr = server_socket.recvfrom(1024)
-        print("Received data from {}: {}".format(addr, data))
-
-
-        # Decode the data and process it
-        try:
-            track_data = json.loads(data.decode('utf-8'))
-            track = Track(track_data['artist'], track_data['song_name'])
-
-            print("Track received: Artist: {}, Song Name: {}".format(track.artist, track.song_name))
-        except Exception as e:
-            print("Error decoding track data: {}".format(e))
-            server_socket.sendto(b"Invalid track data", addr)
-
-# if __name__ == "__main__":
-#     start_server()
-
 class EchoHandler(SocketServer.BaseRequestHandler):
     def handle(self):
         # Read the request data
-        request_data = self.request.recv(1024).strip()
-        print("REQUEST DATA:", request_data)
+        request_data = ''
+        headers = ''
+        body = ''
+        while True:
+            data = self.request.recv(1024)
+            if not data:
+                break
+            
+            headers += data.decode('utf-8')
+            if '}' in data.decode('utf-8'):
+                break
+        
+        print("split",headers.split('\r\n\r\n'))
+        print("body", headers.split('\r\n\r\n')[1])
+        headers, body = headers.split('\r\n\r\n')
 
-        # Find the header-body separator \r\n\r\n
-        headers_end = request_data.find("\r\n\r\n")
-        if headers_end == -1:
-            raise ValueError("No header-body separator found")
+        print("Request headers:", headers)
+        print("Request body:", body)
 
-        headers_str = request_data[:headers_end]  # Extract headers
-        body = request_data[headers_end + 4:]  # Extract body
+        body_dict = json.loads(body)
+        print("body_dict",body_dict)
+        
+        song_name = body_dict.get("song_name")
+        artist = body_dict.get("artist")
+        
+        print("Track stripped: Artist: {}, Song Name: {}".format(artist, song_name))
 
-        # Log headers and body for debugging
-        print("Headers:", headers_str)
-        print("Body:", body)
-
-        # Check if the request method is POST and content type is JSON
-        if "POST" in headers_str and "Content-Type: application/json" in headers_str:
-                track_data = json.loads(body)
-
-                print("Track received:", track_data)
-                
-                artist = track_data.get("artist", "").strip()
-                song_name = track_data.get("song_name", "").strip()
-                
-                print("Track stripped: Artist: {}, Song Name: {}".format(artist, song_name))
-
-                body = '{{ "artist": "{}", "song_name": "{}" }}'.format(artist, song_name)
-                response = "HTTP/1.1 200 OK\r\n" \
-                        "Content-Type: application/json\r\n" \
-                        "Content-Length: {}\r\n" \
-                        "Connection: close\r\n" \
-                        "\r\n" \
-                        "{}".format(len(body), body)
-                print("RESPONSE", response)
+        body = '{{ "artist": "{}", "song_name": "{}" }}'.format(artist, song_name)
+        response = "HTTP/1.1 200 OK\r\n" \
+                "Content-Type: application/json\r\n" \
+                "Content-Length: {}\r\n" \
+                "Connection: close\r\n" \
+                "\r\n" \
+                "{}".format(len(body), body)
+        print("RESPONSE", response)
 
         self.request.sendall(response)
     
+def shutdown(signal, frame):
+    print("\nServer shutting down...")
+    server.server_close()  # Close the server socket
+    sys.exit(0)
+
 if __name__ == "__main__":
+    signal.signal(signal.SIGINT, shutdown)
     server = SocketServer.TCPServer((IP, PORT), EchoHandler)
     print("Echo server started on {}:{}".format(IP,PORT))
-    server.serve_forever()
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print("\nServer interrupted by user")
+        server.server_close()
